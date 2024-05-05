454c7c8dda6bd090860fc12b923aab5a98d13ed0
Integra24 <168156711+Integra24@users.noreply.github.com>
2024-05-04 21:15:30 -0500

    Se adiciona multiply y divide en api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -24,7 +24,26 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "3", "ERROR ADD"
         )
+    def test_api_mulpiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "6", "ERROR ADD"
+        )
 
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/20/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "10", "ERROR ADD"
+        )
+    
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
